61fcd263698acdccfafbef1ffa1ccc867e9577ab
Lara <laragarcia391@gmail.com>
2023-11-03 17:54:33 -0300

    Cambios Lara

---
+++ Créditos/Creditos.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Creditos : MonoBehaviour
+{
+  
+    void Start()
+    {
+        Invoke("WaitToEnd",20);
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+
+    }
+
+
+    public void WaitToEnd()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
