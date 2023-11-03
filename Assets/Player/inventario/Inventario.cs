62dbc482554e2937bf99dfc694f79d857ee953c3
benjamacias <benjamacias01@gmail.com>
2023-11-03 01:25:33 -0300

    agrega inventario

---
+++ Assets/Player/inventario/Inventario.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventario : MonoBehaviour
+{
+    public static Inventario Instance;
+    public List<Item> Items = new List<Item>();
+
+    public Transform ItemContent;
+    public GameObject InventoryItem; 
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void Add(Item item)
+    {
+        Items.Add(item);
+    }
+    public void Remove(Item item)
+    {
+        Items.Remove(item);
+    }
+    public void ListItems()
+    {
+        foreach(var item in Items)
+        {
+            
+            GameObject obj = Instantiate(InventoryItem, ItemContent);
+            Debug.Log(obj.name);
+            //var itemName = obj.transform.Find("ItemName").GetComponent<Text>();
+            //var itemIcon = obj.transform.Find("ItemIcon").GetComponent<Image>();
+            //Debug.Log(itemName);
+            //itemName.name = item.itemName;
+            //itemIcon.prite = item.icon;
+        }
+    }
+}
---
+++ Assets/Player/inventario/itemPick.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class itemPick : MonoBehaviour
+{
+    public Item Item; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    void PickUp()
+    {
+        Inventario.Instance.Add(Item);
+        Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        PickUp();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
