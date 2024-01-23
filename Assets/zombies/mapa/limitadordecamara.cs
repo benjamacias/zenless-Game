1351d6064eee403750f859a55655d730d9842a74
Tadeo Parmigiani <tadeoparmi2001@gmail.com>
2024-01-23 13:36:27 -0300

    minimapa

---
+++ Assets/zombies/mapa/limitadordecamara.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class limitadordecamara : MonoBehaviour
+{
+    public GameObject Player;
+
+    void Update()
+    {
+        transform.position = new Vector3(Player.transform.position.x, 40, Player.transform.position.z);
+    }
+}
